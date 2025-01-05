9c6088e5f88f206cad2980eab935e62738ebc956
Krantikumar _More <kumarmore1266@gmail.com>
2025-01-05 19:51:35 +0530

    Add files via upload

---
+++ lib/assignment.dart
@@ -0,0 +1,52 @@
+// Assignment:
+// 1. Write a Fibbonacci Series program using dart
+
+// void main() {
+//   int n = 10; // Number of terms in the Fibonacci series
+//   int a = 0, b = 1;
+//
+//   print("Fibonacci Series up to $n terms:");
+//   for (int i = 0; i < n; i++) {
+//     print(a);
+//     int next = a + b;
+//     a = b;
+//     b = next;
+//   }
+// }
+
+// 2. Write a program to check if the number is Prime using dart.
+
+// void main() {
+//   int number = 29; // Change the number to check if it is prime
+//
+//   bool isPrime = true;
+//
+//   if (number <= 1) {
+//     isPrime = false;
+//   } else {
+//     for (int i = 2; i <= number ~/ 2; i++) {
+//       if (number % i == 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//   }
+//
+//   if (isPrime) {
+//     print("$number is a Prime Number.");
+//   } else {
+//     print("$number is NOT a Prime Number.");
+//   }
+// }
+
+//// 3. Write a program to check whether the String is palindrome or not
+// void main() {
+//   String str = "radar"; // Change the string to test
+//   String reversedStr = str.split('').reversed.join('');
+//
+//   if (str == reversedStr) {
+//     print("The string '$str' is a Palindrome.");
+//   } else {
+//     print("The string '$str' is NOT a Palindrome.");
+//   }
+// }
